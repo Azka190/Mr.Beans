4d53e4d04232b380f7cf11a96b6b099bea727b73
Azka190 <azkarehman2001@gmail.com>
2024-06-16 17:16:33 +0500

    2

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import Image from 'next/image'
 
 function Services() {
   return (
+
     <div className='bg-custom-blue'>
-        <div>
-        <h1 className='heading heading-color' >My services</h1><br />
+          <div className=" bg-no-repeat  h-screen bg-center  " style={{ backgroundImage: "url('/iPhone 13 Pro.png')" }}>
+
+      <div>
+        <div className='flex absolute w-20 h-15 ml-[650px] pt-20 '> 
+        <img src="bg.png" alt=""  />
         </div>
+        <div className='flex relative'>
+        <h1 className='font-roboto text-[50px] font-[700px]  text-white ml-[670px] pt-[73px]' >My Services </h1><br />
+        </div>
+      </div>
         <div>
-            <img src="iPhone 13 Pro.png" alt="" />
+       </div>
         </div>
     </div>
   )
